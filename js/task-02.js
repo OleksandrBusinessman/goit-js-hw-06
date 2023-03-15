4903db742c79928940fd46a3868809b37403b571
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-03-15 19:36:11 +0100

    doing task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const itemsEl = ingredients.map((ingredient) => {
+  const ingrediensEl = document.createElement("li");
+  ingrediensEl.classList.add("item");
+  ingrediensEl.textContent = ingredient;
+  return ingrediensEl;
+});
+
+const listEl = document.querySelector("#ingredients");
+
+listEl.append(...itemsEl);
